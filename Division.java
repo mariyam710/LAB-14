e8180bbd99fe2b4a50fb1f7a778755a439c8a800
mariyam710 <107102281+mariyam710@users.noreply.github.com>
2022-06-08 12:40:01 +0500

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Division {
+	static Scanner inp = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int num1, num2;
+		System.out.println("====hello kids! welcome to division game======");
+		
+		try {
+			System.out.println("Enter first digit:  ");
+			num1 = inp.nextInt();
+			System.out.println("Enter second digit:  ");
+			num2 = inp.nextInt();
+			if(num2==0) {
+				throw new ArithmeticException("\nZero should not be entered. ");
+			}
+			System.out.println("Answer is :" + num1/num2);
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Only whole numbers should be eneterd. ");
+		}
+			
+		}
+}
+		
+		
+	
+		
+				
+			
+		
+	
+	
+	
+
+
---
+++ Odd262.java
@@ -0,0 +1,11 @@
+
+public class Odd262 {
+	public static boolean odd(int number) {
+		if (number % 2 == 0) {
+			return false;
+		} else {
+			return true;
+
+		}
+	}
+}
